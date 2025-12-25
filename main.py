b8be3f755cd0bf256525267f1844e86027a43d13
bananadesuyo <147511687+bananadesuyo@users.noreply.github.com>
2025-12-25 11:58:54 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from pyscript import document, window
 # ==========================================
 MODEL_LIST = {
     "iwataGPT1.0": "iwataGPT1.0.json",
-    "iwataGPT1.1": "iwataGPT1.1.json"
+    "iwataGPT1.1": "iwataGPT1.1.json",
+    "iwataGPT1.1Pro": "iwataGPT1.1Pro.json"
 }
 
 class MarkovAI:
